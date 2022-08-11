5f7d9d8e9e863f58b3442ae0843a5d7ae7ae9cbf
Colton Rogers <crrogers12@gmail.com>
2022-08-11 13:19:08 -0700

    added comments to helper.go

--- broker-service/cmd/api/helpers.go
+++ broker-service/cmd/api/helpers.go
@@ -25,6 +25,7 @@ func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) er
 		return err
 	}
 
+	//Don't allow multiple json bodies
 	err = dec.Decode(&struct{}{})
 	if err != io.EOF {
 		return errors.New("Body must have only a single JSON value")
@@ -34,19 +35,29 @@ func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) er
 }
 
 func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
+	//adding json to out
 	out, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
 
+	//if there are headers
 	if len(headers) > 0 {
 		for key, value := range headers[0] {
+			// type Header map[string][]string
+			// A Header represents the key-value pairs in an HTTP header.
+			// The keys should be in canonical form, as returned by CanonicalHeaderKey.
 			w.Header()[key] = value
 		}
 	}
-
+	//Setting content-type to header via responsewriter
 	w.Header().Set("Content-Type", "application/json")
+	//I.E. http.StatusOK = 200
+	//call before w.Write()
+	//Writing status code to connection
 	w.WriteHeader(status)
+
+	//Writing json to the connection via responsewriter
 	_, err = w.Write(out)
 	if err != nil {
 		return err
@@ -58,6 +69,7 @@ func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, header
 func (app *Config) errorJSON(w http.ResponseWriter, err error, status ...int) error {
 	statusCode := http.StatusBadRequest
 
+	//If the status is present set status code to status[0]
 	if len(status) > 0 {
 		statusCode = status[0]
 	}
@@ -66,5 +78,6 @@ func (app *Config) errorJSON(w http.ResponseWriter, err error, status ...int) er
 	payload.Error = true
 	payload.Message = err.Error()
 
+	//writing statuscode and error to connection/header
 	return app.writeJSON(w, statusCode, payload)
 }
